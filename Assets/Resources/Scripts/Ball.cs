95592753584b7d9189da497e2c469eeecab7073d
Mun Cheol Ho <cjfgh530@naver.com>
2024-11-29 11:08:30 +0900

    게임 씬 구성2
    * 공 프리팹 추가 및 스크립트 추가

---
+++ Assets/Resources/Scripts/Ball.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public enum BallType
+    {
+        Right,
+        Left,
+        Smash
+    }
+    public float speed = 1.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
